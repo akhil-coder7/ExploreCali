e25f15f34bf55381c0fc3632551e6b0e79444465
akhil-coder7 <ravipati0102@gmail.com>
2021-05-02 21:23:42 -0400

    Intial Project Commit

---
+++ src/main/java/com/example/sbet/bootingweb/ExploreCaliApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbet.bootingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreCaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreCaliApplication.class, args);
+	}
+
+}
